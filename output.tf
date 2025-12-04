4b959637149181d9b8a9e32e49709d105129296d
mooonu <moonudev@gmail.com>
2025-12-04 21:22:43 +0900

    VPC network만 생성하도록

--- output.tf
+++
@@ -1,9 +0,0 @@
-output "bastion_public_ip" {
-  description = "SSH 접속용 Bastion Public IP"
-  value       = aws_instance.bastion.public_ip
-}
-
-output "web_private_ip" {
-  description = "내부 접속용 Web Server Private IP"
-  value       = aws_instance.web.private_ip
-}
